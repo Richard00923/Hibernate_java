2a260da640313ed0ba381c331ce45a5c4426dd48
Richard00923 <Vasyakov6@gmail.com>
2022-12-31 00:53:41 +0200

    aa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Passport.class)
+                .addAnnotatedClass(Card.class)
+                .addAnnotatedClass(Sunglass.class)
+                .getMetadataBuilder().build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(new User("petya", Gender.MALE, Arrays.asList("java", "js", "html"), new Passport("pihfigdfgsdf"), Arrays.asList(new Card("asyfdyqtfwe7765347"), new Card("87542627")), Arrays.asList(new Sunglass("ray" +
+                "ban"), new Sunglass("oko"))));
+
+        session.getTransaction().commit();
+
+
+        session.close();
+        sessionFactory.close();
+
+
     }
 }
