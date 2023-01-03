4736603a5acf7fd70940a26ffe2d2901af1f7b47
Richard00923 <Vasyakov6@gmail.com>
2023-01-03 16:02:06 +0200

    aa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import User.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -7,25 +8,31 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.Arrays;
-
-public class Main {
+public  class Main{
     public static void main(String[] args) {
 
-        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 
-        Metadata metadata = new MetadataSources(serviceRegistry)
-                .getMetadataBuilder().build();
+        StandardServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder()
+                        .configure("hibernate.cfg.xml")
+                        .build();
+
+        Metadata  metadata =
+        new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(User.class)
+                .getMetadataBuilder()
+                .build();
 
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
 
 
 
-           session.close();
+
+
+
+        session.close();
         sessionFactory.close();
-
 
     }
 }
